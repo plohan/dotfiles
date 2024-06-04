344ee2cc1704eaca512352af8d7c9f158c288090
imsozrious <30285202+ImSoZRious@users.noreply.github.com>
2024-06-05 03:37:57 +0700

    chore: change locale to en_US.UTF-8

--- modules/system.nix
+++ modules/system.nix
@@ -8,15 +8,15 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "th_TH.UTF-8";
-    LC_IDENTIFICATION = "th_TH.UTF-8";
-    LC_MEASUREMENT = "th_TH.UTF-8";
-    LC_MONETARY = "th_TH.UTF-8";
-    LC_NAME = "th_TH.UTF-8";
-    LC_NUMERIC = "th_TH.UTF-8";
-    LC_PAPER = "th_TH.UTF-8";
-    LC_TELEPHONE = "th_TH.UTF-8";
-    LC_TIME = "th_TH.UTF-8";
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 
   fonts = {
