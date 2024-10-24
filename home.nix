7e3e45a3670448c4a2a411decf58b2cebfb22618
imsozrious <30285202+ImSoZRious@users.noreply.github.com>
2024-10-24 13:56:51 +0700

    chore: move hyprlock to hyprland conf

--- home.nix
+++ home.nix
@@ -60,60 +60,6 @@
     };
   };
 
-  programs.hyprlock.enable = true;
-  programs.hyprlock.settings = {
-    general = {
-      disable_loading_bar = true;
-      grace = 15;
-      hide_cursor = true;
-      no_fade_in = false;
-    };
-
-    background = [
-      {
-        path = "screenshot";
-        blur_passes = 3;
-        blur_size = 8;
-      }
-    ];
-
-    input-field = [
-      {
-        size = "200, 50";
-        position = "0, -80";
-        monitor = "";
-        dots_center = true;
-        fade_on_empty = false;
-        font_color = "rgb(202, 211, 245)";
-        inner_color = "rgb(91, 96, 120)";
-        outer_color = "rgb(24, 25, 38)";
-        outline_thickness = 5;
-        placeholder_text = "'<span foreground=\"##cad3f5\">Password...</span>'";
-        shadow_passes = 2;
-      }
-    ];
-  };
-  services.hypridle.enable = true;
-  services.hypridle.settings = {
-    general = {
-      after_sleep_cmd = "hyprctl dispatch dpms on";
-      ignore_dbus_inhibit = false;
-      lock_cmd = "pidof hyprlock || hyprlock";
-    };
-  
-    listener = [
-      {
-        timeout = 900;
-        on-timeout = "pidof hyprlock || hyprlock";
-      }
-      {
-        timeout = 1200;
-        on-timeout = "hyprctl dispatch dpms off";
-        on-resume = "hyprctl dispatch dpms on";
-      }
-    ];
-  };
-
   programs.home-manager.enable = true;
   programs.direnv.enable = true;
 
