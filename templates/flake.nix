dca0d7ed4f258642003434f0d754a96bd72bbaf8
imsozrious <30285202+ImSoZRious@users.noreply.github.com>
2024-12-17 04:36:08 +0700

    feat: add template flake

---
+++ templates/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+  { nixpkgs, flake-utils, ... }:
+  flake-utils.lib.eachDefaultSystem (
+    system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    with pkgs; {
+      devShells.nestPrisma = mkShell {
+        packages = [
+          nodePackages.pnpm
+          nodePackages.typescript
+          nodePackages.typescript-language-server
+          nodePackages.prisma
+          nodejs_22
+          nest-cli
+        ];
+        PRISMA_SCHEMA_ENGINE_BINARY="${prisma-engines}/bin/schema-engine";
+        PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine";
+        PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node";
+        PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt";
+      };
+    }
+  );
+}
