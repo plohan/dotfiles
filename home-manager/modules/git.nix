0b7ed1ca702b7b71731e14ab9a4ccb64d2f793c0
ImSoZRious <30285202+ImSoZRious@users.noreply.github.com>
2025-04-15 01:56:55 +0700

    update git

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -2,7 +2,7 @@
 {
   programs.git = {
     enable = true;
-    userName = "imsozrious";
+    userName = "ImSoZRious";
     userEmail = "30285202+ImSoZRious@users.noreply.github.com";
     ignores = [
       ".direnv"
@@ -14,9 +14,37 @@
       noskip = "update-index --no-skip-worktree";
     };
     extraConfig = {
-      pull.rebase = true;
-      merge.conflictstyle = "diff3";
-      merge.tool = "vimdiff";
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+      push = {
+        default = "simple";
+        autoSetupRemote = "true";
+        followTags = true;
+      };
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
+      };
+      merge = {
+        conflictstyle = "zdiff3";
+        tool = "vimdiff";
+      };
+      rebase = {
+        autosquash = true; 
+        autostash = true;
+        updateRefs = true;
+      };
+      pull.rebase = false;
+      commit.verbose = true;
+      grep.patternType = "perl";
       init.defaultBranch = "main";
       url = {
         "ssh://git@github.com" = {
